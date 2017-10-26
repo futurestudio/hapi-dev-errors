1744f54e8e5106c96baf3ada529d3c980ca274b3
Marcus Poehls <marcus.poehls@gmail.com>
2017-10-26 11:13:46 +0200

    add more tests; increase code coverage

--- test/plugin-is-enabled-when-not-in-production.js
+++ test/plugin-is-enabled-when-not-in-production.js
@@ -4,10 +4,11 @@ const Lab = require('lab')
 const Code = require('code')
 const Boom = require('boom')
 const Hapi = require('hapi')
+const Path = require('path')
 
 let server
 
-const { experiment, test, beforeEach } = (exports.lab = Lab.script())
+const { experiment, test, before, beforeEach } = (exports.lab = Lab.script())
 const expect = Code.expect
 
 experiment('hapi-dev-error register plugin', () => {
@@ -100,5 +101,142 @@ experiment('hapi-dev-error register plugin', () => {
     expect(payload.stacktrace).to.exist()
     expect(payload.url).to.equal(routeOptions.path)
     expect(payload.method).to.equal(routeOptions.method)
+  })
+
+  test('test if the request payload is added to the error response', async () => {
+    const routeOptions = {
+      path: '/with-request-payload',
+      method: 'POST',
+      handler: () => {
+        return Promise.reject(new Error('server error with payload'))
+      }
+    }
+
+    server.route(routeOptions)
+
+    const requestPayload = { name: 'Future Studio' }
+    const options = {
+      url: routeOptions.path,
+      method: routeOptions.method,
+      headers: {
+        accept: 'application/json'
+      },
+      payload: requestPayload
+    }
+
+    const response = await server.inject(options)
+    const payload = JSON.parse(response.payload || '{}')
+
+    expect(response.statusCode).to.equal(500)
+    expect(payload.stacktrace).to.exist()
+    expect(payload.payload)
+      .to.be.not.empty()
+      .and.to.equal(requestPayload)
+    expect(payload.url).to.equal(routeOptions.path)
+    expect(payload.method).to.equal(routeOptions.method)
+  })
+})
+
+experiment('hapi-dev-error renders a custom template', () => {
+  before(async () => {
+    server = new Hapi.Server()
+
+    // fake dev env, no process.env.NODE_ENV defined
+    await server.register([
+      {
+        plugin: require('../lib/index'),
+        options: {
+          showErrors: process.env.NODE_ENV !== 'production',
+          template: 'error'
+        }
+      },
+      {
+        plugin: require('vision')
+      }
+    ])
+
+    server.views({
+      engines: {
+        html: require('handlebars')
+      },
+      path: Path.resolve(__dirname, '..', 'examples', 'views'),
+      layout: 'layout'
+    })
+  })
+
+  test('render a template', async () => {
+    const routeOptions = {
+      path: '/custom-view',
+      method: 'GET',
+      handler: () => {
+        return Boom.badImplementation('a custom view rendering error')
+      }
+    }
+
+    server.route(routeOptions)
+
+    const options = {
+      url: routeOptions.path,
+      method: routeOptions.method
+    }
+
+    const response = await server.inject(options)
+    const payload = response.payload
+
+    expect(response.statusCode).to.equal(500)
+    expect(payload).to.startWith('<!DOCTYPE html>')
+    expect(payload).to.contains('Called URL') // this string is part of the custom template
+  })
+})
+
+experiment('hapi-dev-error renders a custom template', () => {
+  before(async () => {
+    server = new Hapi.Server()
+
+    // fake dev env, no process.env.NODE_ENV defined
+    await server.register([
+      {
+        plugin: require('../lib/index'),
+        options: {
+          showErrors: process.env.NODE_ENV !== 'production',
+          template: 'error'
+        }
+      },
+      {
+        plugin: require('vision')
+      }
+    ])
+
+    server.views({
+      engines: {
+        html: require('handlebars')
+      },
+      path: Path.resolve(__dirname, '..', 'examples', 'views'),
+      layout: 'layout'
+    })
+  })
+
+  test('render a template', async () => {
+    const routeOptions = {
+      path: '/custom-view',
+      method: 'GET',
+      handler: () => {
+        return Boom.badImplementation('a custom view rendering error')
+      }
+    }
+
+    server.route(routeOptions)
+
+    const options = {
+      url: routeOptions.path,
+      method: routeOptions.method
+    }
+
+    const response = await server.inject(options)
+    const payload = response.payload
+
+    expect(response.statusCode).to.equal(500)
+    expect(payload).to.startWith('<!DOCTYPE html>')
+    expect(payload).to.contains('Called URL') // this string is part of the custom template
   })
 })
