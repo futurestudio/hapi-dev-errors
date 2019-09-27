73c640eb97c1ac8ceef23897fd8da364ce8be8f1
Marcus Pöhls <marcus.poehls@gmail.com>
2019-09-27 10:12:34 +0200

    revert to joi@15

--- test/plugin-handles-failed-response-validations.js
+++ test/plugin-handles-failed-response-validations.js
@@ -26,9 +26,9 @@ experiment('hapi-dev-error handles failed response validations', () => {
       method: 'GET',
       config: {
         response: {
-          schema: Joi.object({
+          schema: {
             user: Joi.object().required()
-          })
+          }
         }
       },
       handler: () => {
